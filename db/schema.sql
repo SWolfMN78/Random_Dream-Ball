9344a31f37cfd0515d580bf60b3bf18f77ce8751
wzuschlag <wzuschlag@gmail.com>
2018-03-17 13:22:23 -0500

    updated db with names and places

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,12 @@ USE dreamBall_db;
 
 CREATE TABLE dreamBall (
 	id INT (10) AUTO_INCREMENT NOT NULL,
-    characterName VARCHAR (100) NOT NULL,
-    birthPlace VARCHAR (100) NOT NULL,
-    fieldPosition VARCHAR (100) NOT NULL, 
-    illusion VARCHAR (100) NOT NULL,
-    powerPoints INT (10) NOT NULL,
+    charName VARCHAR (100) NOT NULL,
+    homePlanet VARCHAR (100) NOT NULL,
+    powerPoints INT (10) NOT NULL, 
+    specialSkills INT (10) NOT NULL,
+    charCost INT (10) NOT NULL,
+    ownerID INT (10) NOT NULL,
     date TIMESTAMP,
     PRIMARY KEY (id)
-);  
+);   
