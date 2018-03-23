c391594a6183c7b35f78e034dc8b4f15efb52111
Edyta <dudek002@hotmail.com>
2018-03-22 19:37:00 -0500

    E routes added

--- controllers/dreamballsController.js
+++ controllers/dreamballsController.js
@@ -1,18 +1,21 @@
-var express = require("express");
-var router = express.Router();
-
-// Import the model (athlete.js) to use its database functions.
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
 var db = require("../models");
-// Create all our routes and set up logic within those routes where required.
 
-// GET route for getting all of the athletes
+                 
+// Dependencies
+// =============================================================
 
+// grab the orm from the config
+// (remember: connection.js -> orm.js -> route file)
+//var orm = require("../config/orm.js");
 
+// Routes
+// =============================================================
+module.exports = function(app) {
 
-
-///////////////////////////////////////   
-
-router.get("/", function(req, res) {
+  app.get("/", function(req, res) {
   // findAll returns all entries for a table when used with no options
   db.Athlete.findAll({}).then(function(dbResult) {
     var athletes = [];
@@ -20,89 +23,197 @@ router.get("/", function(req, res) {
     dbResult.map(function(athlete) {
       athletes.push(athlete.dataValues);
     });
+    console.log(athletes)
 
     // We have access to the burgers as an argument inside of the callback function
-    res.render('index', { athletes: athletes });
+    res.render('index', athletes);
   });  
-});
-              
-// POST route for saving a new athlete. We can create a athlete using the data on req.body
-router.post("/api/athletes", function(req, res) {
-  console.log(req.body);
+
+});    
+
+  // GET route for getting all of the todos
+  app.get("/api/athletes", function(req, res) {
+
+    // findAll returns all entries for a table when used with no options
+    db.Athlete.findAll({}).then(function(dbResult) {
+      // We have access to the todos as an argument inside of the callback function
+      res.json(dbResult);
+    });
+
+    
+  });
+
+  // POST route for saving a new todo. We can create a todo using the data on req.body
+  app.post("/api/athletes", function(req, res) {
+    console.log(req.body);
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
     db.Athlete.create({
       athleteName: req.body.athleteName,
-      ownerID: req.body.ownerID
+      OwnerId: req.body.ownerID
     }).then(function(dbResult) {
-      // We have access to the new athlete as an argument inside of the callback function
+      // We have access to the new todo as an argument inside of the callback function
       res.json(dbResult);
-    });
-});
-
-// PUT route for updating burgerss. We can access the updated athlete in req.body
-router.put("/api/athletes/:id", function(req, res) {
-  // Update takes in an object describing the properties we want to update, and
-    // we use where to describe which objects we want to update
-  console.log(req.body);
-  db.Athlete.update({
-    athleteName: req.body.athleteName,
-    ownerID: req.body.ownerID
-  }, {
-    where: {
-      id: req.body.id
-    }
-  }).then(function(dbResult) {
-    console.log(dbResult);
-    res.json(dbResult);
+    });   
   });
-});
 
-// DELETE route for deleting athletes. We can access the ID of the burger to delete in
+  
+
+  // DELETE route for deleting todos. We can access the ID of the todo to delete in
   // req.params.id
-router.delete("/api/athletes/:id", function(req, res) {
-  db.Athlete.destroy({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(dbResult) {
-    res.json(dbResult);
-  });
-});
-
-
-/////////////////////////////
-
-router.get("/", function(req, res) {
-  // findAll returns all entries for a table when used with no options
-  db.Owner.findAll({}).then(function(dbResult) {
-    var owners = [];
-
-    dbResult.map(function(owner) {
-      owners.push(owner.dataValues);
-    });
-
-    // We have access to the burgers as an argument inside of the callback function
-    res.render('index', { owners: owners });
-  });
-});
-
-// POST route for saving a new owner. We can create a athlete using the data on req.body
-router.post("/api/owners", function(req, res) {
-  console.log(req.body);
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
-    db.Owner.create({
-      userName: req.body.userName,
-      teamName: req.body.teamName,
-      passWord: req.body.passWord
+  app.delete("/api/athletes/:id", function(req, res) {
+    // We just have to specify which todo we want to destroy with "where"
+    db.Athlete.destroy({
+      where: {
+        id: req.params.id
+      }
     }).then(function(dbResult) {
-      // We have access to the new owner as an argument inside of the callback function
       res.json(dbResult);
     });
-}); 
 
-// Export routes for server.js to use.
-module.exports = router;
+    
+  });
+
+  // PUT route for updating todos. We can access the updated todo in req.body
+  app.put("/api/athletes/:id", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Athlete.update({
+      athleteName: req.body.athleteName,
+      OwnerId: req.body.OwnerId
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbResult) {
+      res.json(dbResult);
+    });
+  });
+
+    
+
+};
+
+
+
+
+
+
+
+
+//    STUFF FOR OTHER TABLES
+//========================================
+                
+  /*// default athletes route is to render all in the database
+  app.get("/api/athletes/team/:OwnerId", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Athlete.findAll({
+      attributes: ['id', 'athleteName', 'homePlanet', 'powerPoints', 'isActive', 'athleteInjured', 'OwnerId'],
+      where: {
+        OwnerId: req.params.OwnerId
+      },
+      // Can only 'join' one table at a time.  We should already know the Owner, so skip it.
+      // include: [{
+      //   model: db.Owner,
+      //   attributes: ['userName', 'teamName']
+      // }],
+      include: [{
+        model: db.SpecialSkill,
+        attributes: ['skillName']
+      }]
+    }).then(function(dbResult) {
+      // console.log('find team result:', dbResult);
+      var athletes = [];
+
+      dbResult.map(function(athlete) {
+        athletes.push(athlete.dataValues);
+      });
+
+      // We have access to the athletes as an argument inside of the callback function
+      res.json({ athletes: athletes });
+    });
+  });
+                
+  // // POST route for saving a new athlete. We can create a athlete using the data on req.body
+  // app.post("/api/athletes", function(req, res) {
+  //   console.log(req.body);
+  //     // create takes an argument of an object describing the item we want to
+  //     // insert into our table. In this case we just we pass in an object with a text
+  //     // and complete property (req.body)
+  //     db.Athlete.create({
+  //       athleteName: req.body.athleteName,
+  //       ownerID: req.body.ownerID
+  //     }).then(function(dbResult) {
+  //       // We have access to the new athlete as an argument inside of the callback function
+  //       res.json(dbResult);
+  //     });
+  // });
+
+  // PUT route for updating athletess. We can access the updated athlete in req.body
+  app.put("/api/athletes/owner/:id", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    // console.log(req.body);
+    db.Athlete.update({
+      // athleteName: req.body.athleteName,
+      OwnerId: req.body.OwnerId
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbResult) {
+      console.log(dbResult);
+      res.json(dbResult);
+    });
+  });
+
+  // PUT route for updating athletess. We can access the updated athlete in req.body
+  app.put("/api/athletes/active/:id", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+      // we use where to describe which objects we want to update
+    console.log(req.body);
+    db.Athlete.update({
+      // athleteName: req.body.athleteName,
+      isActive: req.body.isActive
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbResult) {
+      console.log(dbResult);
+      res.json(dbResult);
+    });
+  });
+
+  // PUT route for updating athletess. We can access the updated athlete in req.body
+  app.put("/api/athletes/injured/:id", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+      // we use where to describe which objects we want to update
+    console.log(req.body);
+    db.Athlete.update({
+      // athleteName: req.body.athleteName,
+      athleteInjured: req.body.injuryTime
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbResult) {
+      console.log(dbResult);
+      res.json(dbResult);
+    });
+  });
+
+  // // DELETE route for deleting athletes. We can access the ID of the athlete to delete in
+  // // req.params.id
+  // app.delete("/api/athletes/:id", function(req, res) {
+  //   db.Athlete.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function(dbResult) {
+  //     res.json(dbResult);
+  //   });
+  // });
+
+*/
--- public/assets/js/dreamballs.js
+++ public/assets/js/dreamballs.js
@@ -1,35 +1,128 @@
+
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-dev").on("click", function(event) {
-    var id = $(this).data("id");
-    var newDev = $(this).data("newdev");
+$(document).ready(function() {
 
-    var newDevState = {
-      ownerID: newDev,
-      id: id
-    };
+  $(document).on("click", "button.delete", deleteDreamBall);
+  $(document).on("click", "button.updateID", changeID);
 
-    // Send the PUT request.
-    $.ajax("/api/athletes/" + id, {
-      type: "PUT",
-      data: newDevState
-    }).then(
-      function() {
-        console.log("changed dev to", newDev);
-        // Reload the page to get the updated list
-        location.reload();
+  // Getting a reference to the input field where user adds a new Athlete
+  var $newItemInput = $("input.new-item");
+  // Our new Athletes will go inside the dreamballContainer
+  var $dreamballContainer1 = $(".dreamball-container1");
+  var $dreamballContainer2 = $(".dreamball-container2");
+  var $dreamballContainer3 = $(".dreamball-container3");
+  var $dreamballContainer4 = $(".dreamball-container4");
+
+  // Our initial Athletes array
+  var  athletes= [];
+
+  // Getting Athletes from database when page loads
+  getDreamBalls();  
+
+  //Adding rows of Athletes
+  function initializeRows() {
+    $dreamballContainer1.empty();
+    $dreamballContainer2.empty();
+    $dreamballContainer3.empty();
+    $dreamballContainer4.empty();
+
+    var rowsToAdd1 = [];
+    var rowsToAdd2 = [];
+    var rowsToAdd3 = [];
+    var rowsToAdd4 = [];
+    for (var i = 0; i < athletes.length; i++) {
+      //console.log("//////" + dreamballs[i].ownerID)
+      switch (athletes[i].OwnerId){
+      case 1:
+      rowsToAdd1.push(createNewRow(athletes[i]));
+      break;
+      case 2:
+      rowsToAdd2.push(createNewRow(athletes[i]));
+      break;
+      case 3:
+      rowsToAdd3.push(createNewRow(athletes[i]));
+      break;
+      case 4:
+      rowsToAdd4.push(createNewRow(athletes[i]));
+      break;
       }
+      
+    }
+    $dreamballContainer1.prepend(rowsToAdd1);
+    $dreamballContainer2.prepend(rowsToAdd2);
+    $dreamballContainer3.prepend(rowsToAdd3);
+    $dreamballContainer4.prepend(rowsToAdd4);
+  }
+
+  // This function gets Athletes from database 
+  function getDreamBalls() {
+    $.get("/api/athletes", function(data) {
+      athletes = data;
+      initializeRows();
+    });
+  }
+
+  // This function deletes an Athlete when the user clicks the delete button
+  function deleteDreamBall(event) {
+    event.stopPropagation();
+    var id = $(this).data("id");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/athletes/" + id
+    }).then(getDreamBalls);
+  }
+
+  function createNewRow(athlete) {
+    var $newInputRow = $(
+      [
+        "<li class='list-group-item dreamball-item'>",
+        "<span>",
+        athlete.athleteName,
+        "</span>",
+        "<input type='text' class='edit' style='display: none;'>",
+        "<button class='delete btn btn-default'>x</button>",
+        "<button class='updateID btn btn-primary btn-default'>>>></button>",
+        "</li>"
+      ].join("")
     );
-  });
+
+    $newInputRow.find("button.delete").data("id", athlete.id);
+    $newInputRow.find("button.updateID").data("athlete", athlete);
+    $newInputRow.data("athlete", athlete);
+    
+    return $newInputRow;
+  }
+
+  
+  function updateDreamBall(athlete) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/athletes",
+      data: athlete
+    }).then(getDreamBalls);
+  }
+
+  function changeID() {
+    var newDreamBall = $(this).data("athlete");
+    console.log(newDreamBall);
+    var newOwnerID = 1;
+    console.log("newOwnerID = " + newOwnerID)
+    newDreamBall.OwnerId = newOwnerID;
+    console.log("newOwnerID.OwnerId = " + newDreamBall.OwnerId);  
+    updateDreamBall(newDreamBall); 
+  }
+
 
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newDreamBall = {
-      charName: $("#bu").val().trim(),
-      ownerID: $("[name=ownerID]:checked").val().trim()
+      athleteName: $("#bu").val().trim(),
+      OwnerId: $("[name=ownerID]:checked").val().trim()
     };
+      
+    //console.log("And the ownerID is = " + newDreamBall.ownerID);
 
     // Send the POST request.
     $.ajax("/api/athletes", {
@@ -56,7 +149,7 @@ $(function() {
     };
 
     // Send the POST request.
-    $.ajax("/api/owners", {
+    $.ajax("/api/owners/new", {
       type: "POST",
       data: newUserID
     }).then(
@@ -68,21 +161,19 @@ $(function() {
     );
   });
 
-  $(".delete-athlete").on("click", function(event) {
+  $(".delete-dreamball").on("click", function(event) {
     var id = $(this).data("id");
-    console.log("deleted athlete");
+    console.log("deleted athletes");
     // Send the DELETE request.
     $.ajax("/api/athletes/" + id, {
       type: "DELETE",
     }).then(
       function() {
-        console.log("deleted athlete", id);
+        console.log("deleted athletes", id);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 });
-
-
 
